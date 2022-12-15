5261cb1c278f6537ddbb318ef0816112cf064e90
Jonathan Rainey <jonathanmichael5150@gmail.com>
2022-12-15 16:01:30 -0600

    Had to reclone the whole thing to include all branches

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -7,11 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
+  memberBeingEdited: object = null;
+
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
+
+  add(memberName: string, isFirst: boolean) {
+
+    // this.crew.push({name: memberName, firstMission: isFirst});
+
+    // console.log("turkeyTest");
+    // console.log(this.crew[0].name);
+    // console.log("turkeyTest");
+
+    let reet = 0;
+    
+    for (let i = 0; i < this.crew.length; i++) {
+      if(this.crew[i].name.includes(memberName)) { 
+        //this.crew.push({name: memberName, firstMission: isFirst});
+        reet++;
+      }
+    }
+
+    if (reet === 0) {
+      this.crew.push({name: memberName, firstMission: isFirst});
+    }
+
+  }  
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
 
   constructor() { }
 
